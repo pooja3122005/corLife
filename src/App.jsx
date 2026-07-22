cd90b519eeffcd1be60ae3ba4ae6d398899df832
pooja3122005 <poojaumanath3@gmail.com>
2026-07-22 22:51:28 +0530

    Initial project upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-900">
+      <h1 className="text-5xl font-bold text-cyan-400">
+        Tailwind CSS is Working 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
+
