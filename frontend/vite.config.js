5037ff435308e097ca8560461d94dbba6e1c359b
pooja3122005 <poojaumanath3@gmail.com>
2026-08-17 20:52:10 +0530

    Fix GitHub Pages base path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,15 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-   base:'/corLife/',
+  base: '/corLife/',
+
   plugins: [
     react(),
     tailwindcss(),
   ],
+
   server: {
     proxy: {
       '/api': {
